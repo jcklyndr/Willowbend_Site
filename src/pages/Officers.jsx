b6bcfe76d8ad62b8003e3319e3d4ce3abe8e24b8
MA. JACKIELYN DUERO <jackielynduero092202@gmail.com>
2026-07-18 22:23:57 +0800

    updated main pages

---
+++ src/pages/Officers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Officers() {
+  return <div></div>;
+}
+
+export default Officers;
---
+++ src/pages/Regulations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Regulations() {
+  return <div></div>;
+}
+
+export default Regulations;
