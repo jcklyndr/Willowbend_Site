38e02b815238a756e720f26f8db1f583ea2b0d10
MA. JACKIELYN DUERO <jackielynduero092202@gmail.com>
2026-09-02 21:04:18 +0800

    updated form style

--- src/components/faq/FaqForm.jsx
+++ src/components/faq/FaqForm.jsx
@@ -9,7 +9,7 @@ function FaqForm() {
 
   return (
     <div
-      className="bg-white rounded-lg shadow-sm p-6 lg:p-8"
+      className="bg-primary rounded-lg shadow-sm p-6 lg:p-8"
       data-aos="fade-up"
       data-aos-duration="1000"
       data-aos-delay="300"
@@ -18,12 +18,10 @@ function FaqForm() {
         <HugeiconsIcon
           icon={BubbleChatQuestionIcon}
           size={28}
-          className="text-primary"
+          className="text-white"
+          strokeWidth={2}
         />
-        <label
-          htmlFor="questionInput"
-          className="font-bold text-secondary text-lg"
-        >
+        <label htmlFor="questionInput" className="text-white font-bold text-lg">
           Have more questions?
         </label>
       </div>
@@ -33,11 +31,11 @@ function FaqForm() {
           id="questionInput"
           rows={10}
           placeholder="Type your question here..."
-          className="w-full rounded-md border border-gray/30 p-3 mb-4 text-text-color focus:outline-none focus:ring-2 focus:ring-tertiary resize-none"
+          className="bg-background w-full rounded-md border border-gray/30 p-3 mb-4 text-text-color placeholder-black/70 focus:outline-none focus:ring-1 focus:ring-white resize-none"
         />
         <button
           type="submit"
-          className="w-full flex items-center justify-center gap-2 bg-primary hover:bg-fourth-color text-white font-bold py-3 rounded-md transition-colors"
+          className="w-full flex items-center justify-center gap-2 bg-secondary hover:bg-fourth-color text-white font-bold py-3 rounded-md transition-colors"
         >
           Submit Question
           <HugeiconsIcon icon={Sent02Icon} size={18} />
