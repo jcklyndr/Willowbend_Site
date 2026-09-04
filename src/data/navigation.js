acb9f365b1152fa4c690c149e9a8d07d33919c58
MA. JACKIELYN DUERO <jackielynduero092202@gmail.com>
2026-09-04 20:10:10 +0800

    transfer directory and renamed files.

--- src/data/navigation.js
+++
@@ -1,8 +0,0 @@
-export const navigation = [
-  { name: "Home", path: "/" },
-  { name: "About", path: "/about" },
-  { name: "Rules & Regulations", path: "/regulations" },
-  { name: "Officers", path: "/officers" },
-  { name: "FAQs", path: "/faqs" },
-  { name: "Contact", path: "/contact" },
-];
--- src/pages/Privacy.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Privacy() {
-  return <div></div>;
-}
-
-export default Privacy;
